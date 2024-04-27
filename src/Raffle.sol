4194972fb5753aadbcec5cc6b48fe5e141e4f4c9
Marcx <haitianhero6@gmail.com>
2024-04-27 08:24:30 -0400

    raffle

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,8 +2,6 @@
 
 pragma solidity ^0.8.18;
 
-error Raffle__NotEnoughEth();
-
 
 /**
  * @title A sample raffle contract
@@ -14,17 +12,21 @@ error Raffle__NotEnoughEth();
 
 
 contract Raffle {
+    error Raffle__NotEnoughEth();
+
 
     uint256 private  immutable i_entranceFee;
+    address payable[] private s_players;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
     function enterRaffle() external payable {
-        if(msg.value >= i_entranceFee) {
+        if(msg.value < i_entranceFee) {
             revert Raffle__NotEnoughEth();
         }
+        s_players.push(payable(msg.sender));
 
     }
 
@@ -35,6 +37,16 @@ contract Raffle {
 
     function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
+    }
+
+    function getPlayerCount() external view returns (uint256) {
+        return s_players.length;
+    }
+    function getPlayers() external view returns (address[] memory) {
+        return s_players;
+    }
+    function getPlayer(uint256 index) external view returns (address) {
+        return s_players[index];
     }
 
 
