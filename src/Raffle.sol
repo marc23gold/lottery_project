b76f36126a62a0081c5854f1114e10d7ba9396b1
Marcx <haitianhero6@gmail.com>
2024-04-27 09:35:10 -0400

    raffle vrf and enum

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,6 +2,9 @@
 
 pragma solidity ^0.8.18;
 
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 
 /**
  * @title A sample raffle contract
@@ -11,27 +14,77 @@ pragma solidity ^0.8.18;
  */
 
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     error Raffle__NotEnoughEth();
+    error Raffle__TransactionFailed();
+    error Raffle__RaffleNotOpen(); 
+
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
 
 
+    uint16 private constant requestConfirmations = 3;
+    uint32 private constant numWords = 1;
+    uint32 private immutable i_callbackGasLimit;
     uint256 private  immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_keyHash;
+    uint64 private immutable i_subscriptionId;
+    address private s_recentWinner;
+    RaffleState private s_raffleState;
 
-    constructor(uint256 entranceFee) {
+    //@dev duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    uint256 private s_lastTimeStamp;
+
+    event EnteredRaffle(address indexed player);
+
+    constructor(uint256 entranceFee, uint256 interval, address vrfCoordinator, bytes32 keyHash, uint64 subscriptionId,
+    uint32 callbackGasLimit) VRFConsumerBaseV2(vrfCoordinator){
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_keyHash = keyHash;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+        s_raffleState = RaffleState.OPEN;
     }
 
     function enterRaffle() external payable {
         if(msg.value < i_entranceFee) {
             revert Raffle__NotEnoughEth();
         }
+        if(s_raffleState != RaffleState.OPEN) {
+            revert();
+        }
         s_players.push(payable(msg.sender));
-
+        emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() public {
+    function pickWinner() external {
+        if(block.timestamp - s_lastTimeStamp <= i_interval) {
+            revert Raffle__RaffleNotOpen();
+        }
+        uint256 s_requestId = i_vrfCoordinator.requestRandomWords(
+            i_keyHash, //gas lane
+            i_subscriptionId, //address you've funded with link
+            requestConfirmations,
+            i_callbackGasLimit,
+            numWords //number of random numbers
+        );
+    }
 
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override{
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable winner = s_players[indexOfWinner];
+        s_recentWinner = winner;
+        (bool success, ) = s_recentWinner.call{value: address(this).balance}("");
+        if(!success) {
+            revert Raffle__TransactionFailed();
+        }
     }
 
 
@@ -41,9 +94,6 @@ contract Raffle {
 
     function getPlayerCount() external view returns (uint256) {
         return s_players.length;
-    }
-    function getPlayers() external view returns (address[] memory) {
-        return s_players;
     }
     function getPlayer(uint256 index) external view returns (address) {
         return s_players[index];
